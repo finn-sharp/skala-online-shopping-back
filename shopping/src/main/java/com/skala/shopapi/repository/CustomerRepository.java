c0dcc11bd7c8d3cd1a6b1af1a48b9b8f8203076c
finn-sharp <penguin-klg@naver.com>
2026-08-08 15:04:00 +0900

    feat:ProductController String Return으로 Controller frame 구현 Controller에서 CRUD를 통해 DB 업데이트하는 controller 기본 틀을 작성함, 추후 서비스 구현시 정상적으로 동작 예정 #8

--- shopping/src/main/java/com/skala/shopapi/repository/CustomerRepository.java
+++ shopping/src/main/java/com/skala/shopapi/repository/CustomerRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skala.shopapi.data.table.Customer;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long>{
+public interface CustomerRepository extends JpaRepository<Customer, String>{
     
 }
--- shopping/src/test/java/com/skala/shopapi/ShoppingApplicationTests.java
+++ shopping/src/test/java/com/skala/shopapi/ShoppingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.skala.shopping;
+package com.skala.shopapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,5 +9,4 @@ class ShoppingApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
