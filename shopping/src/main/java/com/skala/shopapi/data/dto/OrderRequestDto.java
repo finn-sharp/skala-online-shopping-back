e12be73eb7c4d836d95c7367b3fa3da5d8a10000
finn-sharp <penguin-klg@naver.com>
2026-08-09 15:08:14 +0900

    feat:Valid Annotation & Response Warpping - Valid Annotation 적용을 통해 함수의 신뢰성을 높임 - Response를 wrapping하여 entity, dto를 직접 받아 처리하는 부분에 필요한 후처리 부분을 controller에서 return 할 수 있도록 보완함 #27

--- shopping/src/main/java/com/skala/shopapi/data/dto/OrderRequestDto.java
+++ shopping/src/main/java/com/skala/shopapi/data/dto/OrderRequestDto.java
@@ -1,5 +1,7 @@
 package com.skala.shopapi.data.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +17,7 @@ public class OrderRequestDto {
     @NotNull(message = "고객 ID는 필수입니다")
     private String customerId;
 
-    @NotNull(message = "주문 상품 목록은 필수입니다")
+    @NotEmpty(message = "주문 상품 목록은 필수입니다")
+    @Valid // List 내부의 OrderItemDto 필드들에 대한 @Valid 검증을 활성화합니다.
     private List<OrderItemDto> orderItems;
 }
